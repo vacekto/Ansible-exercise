6381fcd7673f072cbff4322a4d1a5bbcb85b9f12
Tomas Vacek <tomas.vacek@signageos.io>
2026-07-15 16:01:03 +0200

    Configure setup.sh script and dependencies

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e 
+
+WORKDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$WORKDIR"
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if ! command_exists ansible-playbook; then
+    echo "Installing Ansible..."
+    sudo apt update
+    sudo apt install -y ansible
+fi
+
+if ! ansible-galaxy collection list | grep -q "community.docker"; then
+    echo "Installing Ansible Docker collection..."
+    ansible-galaxy collection install community.docker
+fi
+
+echo "Running Ansible server..."
+ansible-playbook -i inventory site.yaml -K
+
+echo "Deployment completed."
